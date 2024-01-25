0cf10858c310446afa9e3776b977742e1962a093
Boyeon Ihn <boyeonihn@gmail.com>
2024-01-25 14:40:21 +0900

    feat: create index '/' page with signout feature

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) console.log(error);
+};
+console.log(user);
+</script>
+<template>
+  <div v-if="user">
+    <UButton @click="signOut">Signout</UButton>
+  </div>
+
+  <h1 class="font-bold text-xl">HELLO</h1>
+</template>

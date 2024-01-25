1ca69d1a46465431af06079a000f5fcc9e608ef8
Boyeon Ihn <boyeonihn@gmail.com>
2024-01-25 14:40:02 +0900

    feat: create login feature

---
+++ pages/login.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import type { FormError, FormSubmitEvent } from '#ui/types';
+
+const supabase = useSupabaseClient();
+
+const state = reactive({
+  email: undefined,
+  password: undefined,
+});
+
+const onEmailLogin = async (event: FormSubmitEvent<any>) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: 'example@email.com',
+    password: 'example-password',
+  });
+};
+
+const validate = (state: any): FormError[] => {
+  const errors = [];
+  if (!state.email) errors.push({ path: 'email', message: 'Required' });
+  if (!state.password) errors.push({ path: 'password', message: 'Required' });
+  return errors;
+};
+</script>
+
+<template>
+  <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <h2 class="my-6 text-center text-3xl font-extrabold u-text-white">
+      Sign in to your account
+    </h2>
+    <LoginCard>
+      <UForm :state="state" :validate="validate" @submit="onEmailLogin">
+        <UFormGroup label="Email" name="email">
+          <UInput v-model="state.email" />
+        </UFormGroup>
+        <UFormGroup label="Password" name="password">
+          <UInput v-model="state.password" type="password" />
+        </UFormGroup>
+        <UButton type="submit"> Login </UButton>
+      </UForm>
+      <UButton
+        class="mt-3"
+        icon="i-mdi-github"
+        block
+        label="Github"
+        @click="supabase.auth.signInWithOAuth({ provider: 'github' })"
+      />
+      <UButton
+        class="mt-3"
+        icon="i-mdi-google"
+        block
+        label="Google"
+        @click="supabase.auth.signInWithOAuth({ provider: 'google' })"
+      />
+    </LoginCard>
+  </div>
+</template>

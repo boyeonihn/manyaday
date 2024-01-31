5b102d9265715f8f1f1af10dc3f9a83afb8bbb7e
Boyeon Ihn <boyeonihn@gmail.com>
2024-01-31 12:12:23 +0900

    feat: create /today route page

---
+++ pages/today.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const date = ref(Date.now());
+</script>
+<template>
+  <h1>today is</h1>
+  <h2>[ {{ date }}]</h2>
+  <LogoutButton />
+  <p>Here are all the diaries from this MM-DD date.</p>
+</template>

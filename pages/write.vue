45944b28819e8c8e5c16d3db04bcc46f8f9edef9
Boyeon Ihn <boyeonihn@gmail.com>
2024-02-06 21:20:44 +0900

    feat: create write form

--- pages/write.vue
+++ pages/write.vue
@@ -1,18 +1,56 @@
 <script setup lang="ts">
+import type { FormError, FormSubmitEvent } from '#ui/types';
 import { format } from 'date-fns';
 
-const date = ref(new Date());
+const state = reactive({
+  date: new Date(),
+  content: '',
+  isPrivate: false,
+});
+
+const validate = (state: any): FormError[] => {
+  const errors = [];
+  if (!state.content)
+    errors.push({
+      path: 'content',
+      message: "It's an empty post!",
+    });
+  return errors;
+};
+
 </script>
 
 <template>
-  <UPopover overlay mode="hover" :popper="{ placement: 'bottom-start' }">
-    <UButton
-      icon="i-heroicons-calendar-days-20-solid"
-      :label="format(date, 'd MMM, yyy')"
+  writing a post for [[ {{ format(state.date, 'MMM do, yyy') }} ]]
+  <UForm
+    :validate="validate"
+    :state="state"
+    class="space-y-4"
+    @submit="onSubmit"
+  >
+    <UPopover overlay mode="click" :popper="{ placement: 'bottom-start' }">
+      <UButton
+        icon="i-heroicons-calendar-days-20-solid"
+        :label="format(state.date, 'MMM do, yyy')"
+      />
+      <template #panel="{ close }">
+        <DatePicker v-model="state.date" @close="close" />
+      </template>
+    </UPopover>
+    <UCheckbox
+      v-model="state.isPrivate"
+      name="isPrivate"
+      label="Make post public"
     />
+    <UFormGroup label="Content" name="content">
+      <UTextarea
+        v-model="state.content"
+        variant="outline"
+        placeholder="Tell me about your day..."
+      />
+    </UFormGroup>
 
-    <template #panel="{ close }">
-      <DatePicker v-model="date" @close="close" />
-    </template>
-  </UPopover>
+    <UButton @click="savePost" type="button">Save Draft</UButton>
+    <UButton type="submit"> Publish </UButton>
+  </UForm>
 </template>

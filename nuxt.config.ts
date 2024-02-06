af60e1c19439061530c3083f8f441bb85e704ecf
Boyeon Ihn <boyeonihn@gmail.com>
2024-02-06 21:21:27 +0900

    style: add global css styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
       baseUrl: process.env.BASE_URL || 'http://localhost:3000',
     },
   },
+  css: ['@/assets/css/main.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxtjs/supabase'],
   supabase: {

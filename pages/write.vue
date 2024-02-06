e309dd3ca3168039178779cdf30427c6487aa7b6
Boyeon Ihn <boyeonihn@gmail.com>
2024-02-06 14:32:08 +0900

    feat: incorporate DatePicker component in write page

--- pages/write.vue
+++ pages/write.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { format } from 'date-fns';
+
+const date = ref(new Date());
+</script>
 
 <template>
-  <h1>WRITE</h1>
+  <UPopover overlay mode="hover" :popper="{ placement: 'bottom-start' }">
+    <UButton
+      icon="i-heroicons-calendar-days-20-solid"
+      :label="format(date, 'd MMM, yyy')"
+    />
+
+    <template #panel="{ close }">
+      <DatePicker v-model="date" @close="close" />
+    </template>
+  </UPopover>
 </template>

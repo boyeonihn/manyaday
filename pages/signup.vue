c3676be1600ecefc3bdb2b0ba6c06cf90c07d6e0
Boyeon Ihn <boyeonihn@gmail.com>
2024-02-13 12:12:18 +0900

    chore: specify database type for supabaseClient

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { FormError, FormSubmitEvent } from '#ui/types';
+import type { Database } from '@/types/database.types';
 
-const supabase = useSupabaseClient();
+const supabase = useSupabaseClient<Database>();
 
 const state = reactive({
   email: undefined,

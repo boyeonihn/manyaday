6645ca9f06e3264f00de86df4b7e0fdf762fa0ba
Boyeon Ihn <95451902+boyeonihn@users.noreply.github.com>
2024-02-20 19:07:28 +0900

    feat: create write entry page (#9)
    * feat: create write page
    
    * feat: create DatePicker component
    
    * feat: incorporate DatePicker component in write page
    
    * feat: create write form
    
    * feat: update date-picker UI and limit choice of date range
    
    * style: add global css styling
    
    * chore: add .tool-versions
    
    * build: create public.entries db table
    
    * chore: add build:types command for supabase typescript typing
    
    * build: update entries db
    
    * chore: rename supabase.ts to db.types.ts
    
    * chore: rename 'state' to 'entry'
    
    * feat: insert entry to supabase upon submission
    
    * chore: specify database type for supabaseClient
    
    * chore: update package-lock.json
    
    * chore: download supabase-js
    
    * fix: update earliest-date for date picker
    
    * feat: format date on today page
    
    * chore: remove redundant date element

--- pages/today.vue
+++ pages/today.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { format } from 'date-fns';
 
-const date = ref(Date.now());
+const date = Date.now();
 </script>
 <template>
   <h1>today is</h1>
-  <h2>[ {{ date }}]</h2>
+  <h2>{{ format(date, 'MMM do, yyy') }}</h2>
   <LogoutButton />
   <p>Here are all the diaries from this MM-DD date.</p>
 </template>
---
+++ pages/write.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import type { FormError, FormSubmitEvent } from '#ui/types';
+import type { Database } from '@/types/database.types';
+import { format } from 'date-fns';
+
+const supabase = useSupabaseClient<Database>();
+const user = useSupabaseUser();
+const user_id = user.value!.id;
+
+const entry = reactive({
+  date: new Date(),
+  content: '',
+  isPrivate: false,
+});
+
+let status: 'init' | 'submitting' | 'error' = 'init';
+
+const validate = (state: any): FormError[] => {
+  const errors = [];
+  if (!entry.content)
+    errors.push({
+      path: 'content',
+      message: "It's an empty post!",
+    });
+  return errors;
+};
+
+const savePost = () => {
+  console.log('click save');
+};
+
+const createEntry = async (event: FormSubmitEvent<any>) => {
+  // need to reach supabase to add entry
+  if (entry.content.trim().length === 0) {
+    return;
+  }
+
+  const { data, error } = await supabase.from('entries').insert({
+    content: entry.content,
+    is_private: entry.isPrivate,
+    created_ts: entry.date.toDateString(),
+    user_id,
+  });
+  if (error) {
+    status = 'error';
+    return;
+  } else {
+    navigateTo('/today'); // redirecting to /today for now
+  }
+};
+</script>
+
+<template>
+  <div v-if="status === 'error'">{{ 'there is something wrong' }}</div>
+  <UForm
+    :validate="validate"
+    :state="entry"
+    class="space-y-4"
+    @submit="createEntry"
+  >
+    <UPopover overlay mode="click" :popper="{ placement: 'bottom-start' }">
+      <span
+        >writing a post for
+        <UButton
+          icon="i-heroicons-calendar-days-20-solid"
+          :label="'[ [  ' + format(entry.date, 'MMM do, yyy') + '  ] ]'"
+      /></span>
+      <template #panel="{ close }">
+        <DatePicker v-model="entry.date" @close="close" />
+      </template>
+    </UPopover>
+    <UCheckbox
+      v-model="entry.isPrivate"
+      name="isPrivate"
+      label="Private post"
+    />
+    <UFormGroup label="Content" name="content">
+      <UTextarea
+        v-model="entry.content"
+        variant="outline"
+        placeholder="Tell me about your day..."
+      />
+    </UFormGroup>
+
+    <UButton @click="savePost" type="button">Save Draft</UButton>
+    <UButton type="submit"> Publish </UButton>
+  </UForm>
+</template>

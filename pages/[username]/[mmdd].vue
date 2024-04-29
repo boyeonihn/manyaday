9dfb5d399db7237487c0ba51c486b8af5f08a434
Boyeon Ihn <boyeonihn@gmail.com>
2024-04-29 19:18:37 +0900

    feat: create mmdd dynamic route page

---
+++ pages/[username]/[mmdd].vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+let {
+  params: { mmdd, username },
+} = useRoute();
+
+const monthDay = `${mmdd.slice(0, 2)}-${mmdd.slice(2)}`;
+username = typeof username === 'string' ? username : username[0];
+
+const userId = (await useFindUser(username)) as string;
+const entries = await useEntries({ userId, monthDay });
+
+definePageMeta({
+  validate: async (route) => {
+    let { username, mmdd } = route.params;
+    username = typeof username === 'string' ? username : username[0];
+    if (mmdd.length != 4) return false;
+
+    const fetchedUserId = await useFindUser(username);
+    if (!fetchedUserId) return false;
+
+    return true;
+  },
+});
+</script>
+<template>
+  <h1>{{ username }}'s entries for {{ monthDay }}</h1>
+  <ul>
+    <li v-for="entry in entries" :key="entry.id">
+      <EntryUnit :content="entry.content" :createdAt="entry.created_ts" />
+    </li>
+  </ul>
+</template>
